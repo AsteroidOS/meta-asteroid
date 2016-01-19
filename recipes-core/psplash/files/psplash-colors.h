25fad955a80ef4f241be6e3479966530a3c1d6ee
Florent Revest <revestflo@gmail.com>
2016-01-19 22:27:27 +0100

    psplash: adapt the bootsplash to the asteroid-launcher's shutdown screen

--- recipes-core/psplash/files/psplash-colors.h
+++ recipes-core/psplash/files/psplash-colors.h
@@ -20,7 +20,7 @@
 #define _HAVE_PSPLASH_COLORS_H
 
 /* This is the overall background color */
-#define PSPLASH_BACKGROUND_COLOR 0xec,0xec,0xec
+#define PSPLASH_BACKGROUND_COLOR 0x00,0x00,0x00
 
 /* This is the color of any text output */
 #define PSPLASH_TEXT_COLOR 0x6d,0x6d,0x6d
@@ -29,6 +29,6 @@
 #define PSPLASH_BAR_COLOR 0x6d,0x6d,0x6d
 
 /* This is the color of the progress bar background */
-#define PSPLASH_BAR_BACKGROUND_COLOR 0xec,0xec,0xec
+#define PSPLASH_BAR_BACKGROUND_COLOR 0x00,0x00,0x00
 
 #endif
